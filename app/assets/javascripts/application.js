bad00ee02ff4310cca157435149ae3ce47a20799
Nicolas Duchemin <duchemin.nicolas@gmail.com>
2017-04-06 15:49:47 +0900

    installed Semantic UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require semantic-ui
+//= require bootstrap-sprockets
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'cocktails#index'
   resources :cocktails, only: [:new, :index, :create, :show, :destroy] do
     resources :doses, only: [:new, :create, :destroy]
   end
